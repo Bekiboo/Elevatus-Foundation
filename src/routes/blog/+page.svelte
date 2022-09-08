0b75155a6715d7731aa49bc5887b38d319405697
Bekiboo <julien.connault@gmail.com>
2022-09-08 10:24:21 +0300

    blog cards

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -18,9 +18,29 @@
 
 <Hero {...hero} />
 
-<section class="min-h-[50vh]">
+<section class="container min-h-[50vh]">
   {#each data.data as blogPost}
-    <div class="container py-8">
+    <a
+      href="#"
+      class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100"
+    >
+      <img
+        class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src={blogPost.img} alt={blogPost.title}
+      />
+      <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+        >
+        {blogPost.title}
+        </h5>
+        <p class="mb-3 font-normal text-gray-700">
+          {blogPost.body}
+        </p>
+      </div>
+    </a>
+
+    <!-- <div class="container py-8">
       <ul>
         <li class="pt-2"><p>{blogPost.title}</p></li>
         <li class="pt-2"><p>{blogPost.caption}</p></li>
@@ -29,6 +49,6 @@
         <li class="pt-2"><p>{blogPost.archived}</p></li>
         <li class="pt-2"><p>{blogPost.created_at}</p></li>
       </ul>
-    </div>
+    </div> -->
   {/each}
 </section>

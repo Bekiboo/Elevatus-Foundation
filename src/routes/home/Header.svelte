f4c1f86e6e480c6949f312e7c749aa81a71da95b
Bekiboo <julien.connault@gmail.com>
2022-09-10 16:10:23 +0300

    preload video test 2

--- src/routes/home/Header.svelte
+++ src/routes/home/Header.svelte
@@ -5,24 +5,26 @@
   let innerWidth
   let paused
 
-  function handleResize() {
-    if (innerWidth > 640) {
-      paused = false
-    }
-  }
+  // function handleResize() {
+  //   if (innerWidth > 640) {
+  //     paused = false
+  //   }
+  // }
 
-  onMount(() => {
-    if (innerWidth > 640) {
-      paused = false
-    }
-  })
+  // onMount(() => {
+  //   if (innerWidth > 640) {
+  //     paused = false
+  //   }
+  // })
 </script>
 
-<svelte:window on:resize={handleResize} bind:innerWidth />
+<svelte:window bind:innerWidth />
 <div
   class="flex flex-col justify-end items-center relative w-full text-white overflow-hidden"
 >
   <!-- Desktop Hero -->
+  {#if innerWidth > 640}
+    
   <div
     class="texture smm:hidden h-[80vh] sm:min-h-[400px] w-full absolute z-10"
   />
@@ -30,14 +32,13 @@
     class="smm:hidden object-cover h-[80vh] sm:min-h-[400px] w-full brightness-50"
     loop
     muted
-    preload="none"
-    autoplay={innerWidth > 640 ? true : null}
-    bind:paused
+    autoplay
   >
     <!-- A CORRIGER, VIDEO NE CHARGE PLUS EN GRAND ECRAN, innerWidth a une valeur de 0 au chargement -->
     <source src="img/Elevatus_loop_large.mp4" type="video/mp4" />
     <div class="texture top-0 left-0 w-[100vw] h-[100vh]" />
   </video>
+  {/if}
 
   <!-- Mobile Hero -->
   <img

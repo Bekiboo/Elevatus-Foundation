7ad9b2a32b6341ee90409f7fbe1ef1688bf1474f
Bekiboo <julien.connault@gmail.com>
2023-10-12 13:48:20 +0300

    fix blogpost not showing up

--- src/routes/(public)/blog/+page.js
+++ src/routes/(public)/blog/+page.js
@@ -1,17 +1,21 @@
-import { getPosts } from '$lib/db/blog'  
-import { blogPosts } from '$lib/stores/blog'
+import { getPosts } from '$lib/db/blog';
+import { blogPosts } from '$lib/stores/blog';
 
-
-let posts
+let posts;
 blogPosts.subscribe((value) => {
-  posts = value
-})
+  posts = value;
+});
 
 export const load = async () => {
   // If the blogPosts store is empty, then fetch blog posts from DB
-  if (posts.length == 0) {
-    let data = await getPosts()
-
-    blogPosts.set(data)
+  if (posts != null) {
+    await getPosts()
+      .then((res) => {
+        blogPosts.set(res);
+      })
+      .catch((err) => {
+        blogPosts.set(null);
+        console.log(err);
+      });
   }
-}
+};
--- src/routes/(public)/blog/+page.svelte
+++ src/routes/(public)/blog/+page.svelte
@@ -36,77 +36,83 @@
 
 <section class="container min-h-[50vh]">
   <div class="w-full">
-    {#each $blogPosts as blogPost, i}
-      {#if i == 0}
-        <div
-          class="flex flex-col mx-auto max-w-2xl md:flex-row lg:max-w-6xl my-16"
-        >
-          <a
-            href="/blog/{blogPost.id}"
-            class="basis-1/2 hover:opacity-80 duration-100 flex-none"
-          >
-            <img
-              class="object-cover w-full h-96 md:w-full"
-              src={blogPost?.elements[0].value}
-              alt={blogPost?.title}
-            />
-          </a>
-          <div class="flex flex-col p-4 leading-normal">
-            <a href="/blog/{blogPost.id}"
-              ><h5
-                class="mb-2 text-4xl font-bold tracking-tight text-gray-900 hover:text-orange-500"
-              >
-                {blogPost.title}
-              </h5></a
-            >
-            <p class="mb-3 font-normal text-gray-700">
-              {blogPost.caption != null ? blogPost.caption : ''}
-            </p>
-            <div>
-              {makeExcerpt(blogPost, 400)}
-              <a href="/blog/{blogPost.id}" class="hover:text-orange-500"
-                >[...]</a
-              >
-            </div>
-            <div class="pt-2"><p>{getDate(blogPost.created_at)}</p></div>
-          </div>
-        </div>
-      {/if}
-    {/each}
-
-    <div class="flex gap-x-8 gap-y-16 flex-wrap">
+    {#if $blogPosts != null}
       {#each $blogPosts as blogPost, i}
-        {#if i != 0}
-          <div class="flex flex-col mx-auto md:max-w-xs">
-            <a href="/blog/{blogPost?.id}" class="hover:opacity-80 duration-100">
+        {#if i == 0}
+          <div
+            class="flex flex-col mx-auto max-w-2xl md:flex-row lg:max-w-6xl my-16"
+          >
+            <a
+              href="/blog/{blogPost.id}"
+              class="basis-1/2 hover:opacity-80 duration-100 flex-none"
+            >
               <img
-                class="object-cover w-full h-96 md:h-auto md:w-full"
+                class="object-cover w-full h-96 md:w-full"
                 src={blogPost?.elements[0].value}
                 alt={blogPost?.title}
               />
             </a>
             <div class="flex flex-col p-4 leading-normal">
-              <a href="/blog/{blogPost?.id}"
+              <a href="/blog/{blogPost.id}"
                 ><h5
                   class="mb-2 text-4xl font-bold tracking-tight text-gray-900 hover:text-orange-500"
                 >
-                  {blogPost?.title}
+                  {blogPost.title}
                 </h5></a
               >
               <p class="mb-3 font-normal text-gray-700">
-                {blogPost?.caption != null ? blogPost?.caption : ''}
+                {blogPost.caption != null ? blogPost.caption : ''}
               </p>
               <div>
-                {makeExcerpt(blogPost, 200)}
-                <a href="/blog/{blogPost?.id}" class="hover:text-orange-500"
+                {makeExcerpt(blogPost, 400)}
+                <a href="/blog/{blogPost.id}" class="hover:text-orange-500"
                   >[...]</a
                 >
               </div>
-              <div class="pt-2"><p>{getDate(blogPost?.created_at)}</p></div>
+              <div class="pt-2"><p>{getDate(blogPost.created_at)}</p></div>
             </div>
           </div>
         {/if}
       {/each}
-    </div>
+
+      <div class="flex gap-x-8 gap-y-16 flex-wrap">
+        {#each $blogPosts as blogPost, i}
+          {#if i != 0}
+            <div class="flex flex-col mx-auto md:max-w-xs">
+              <a href="/blog/{blogPost?.id}" class="hover:opacity-80 duration-100">
+                <img
+                  class="object-cover w-full h-96 md:h-auto md:w-full"
+                  src={blogPost?.elements[0].value}
+                  alt={blogPost?.title}
+                />
+              </a>
+              <div class="flex flex-col p-4 leading-normal">
+                <a href="/blog/{blogPost?.id}"
+                  ><h5
+                    class="mb-2 text-4xl font-bold tracking-tight text-gray-900 hover:text-orange-500"
+                  >
+                    {blogPost?.title}
+                  </h5></a
+                >
+                <p class="mb-3 font-normal text-gray-700">
+                  {blogPost?.caption != null ? blogPost?.caption : ''}
+                </p>
+                <div>
+                  {makeExcerpt(blogPost, 200)}
+                  <a href="/blog/{blogPost?.id}" class="hover:text-orange-500"
+                    >[...]</a
+                  >
+                </div>
+                <div class="pt-2"><p>{getDate(blogPost?.created_at)}</p></div>
+              </div>
+            </div>
+          {/if}
+        {/each}
+      </div>
+    {:else}
+      <div class="w-[60ch] mx-auto mt-12">
+        <p>Oops! We couldn't load the blog posts. Please check your internet connection and try again. If the problem persists, please contact our support team for assistance.</p>
+      </div>
+    {/if}
   </div>
 </section>
